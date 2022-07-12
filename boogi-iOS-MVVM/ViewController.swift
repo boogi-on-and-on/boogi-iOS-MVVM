88eda8d149f484e302f2c3e16dc4bf07d94eabbb
DeokHwan Kim <30774991+tiger1710@users.noreply.github.com>
2022-07-12 12:12:44 +0900

    Initial Commit

---
+++ boogi-iOS-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  boogi-iOS-MVVM
+//
+//  Created by 김덕환 on 2022/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ boogi-iOS-MVVMUITests/boogi_iOS_MVVMUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  boogi_iOS_MVVMUITestsLaunchTests.swift
+//  boogi-iOS-MVVMUITests
+//
+//  Created by 김덕환 on 2022/07/12.
+//
+
+import XCTest
+
+class boogi_iOS_MVVMUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
